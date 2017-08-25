24575eb45ef00f8bd87498f78524430b8fc3ccee
wfg <431819864@qq.com>
2017-08-25 09:58:19 +0800

    第二次提交e3-manager

--- e3-manager/e3-manager-service/src/main/java/com/e3mall/service/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/com/e3mall/service/ItemServiceImpl.java
@@ -1,10 +1,14 @@
 package com.e3mall.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.e3mall.mapper.TbItemMapper;
 import com.e3mall.pojo.TbItem;
+import com.e3mall.pojo.TbItemExample;
+import com.e3mall.pojo.TbItemExample.Criteria;
 @Service
 public class ItemServiceImpl implements ItemService{
 
@@ -12,7 +16,16 @@ public class ItemServiceImpl implements ItemService{
 	private TbItemMapper tbItemMapper;
 	
 	public TbItem getItemById(Long itemId){
-		TbItem tbItem = tbItemMapper.selectByPrimaryKey(itemId);
-		return tbItem;
+		//TbItem tbItem = tbItemMapper.selectByPrimaryKey(itemId);
+		TbItemExample example=new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		//设置查询条件
+		criteria.andIdEqualTo(itemId);
+		//执行查询
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		if (list != null && list.size() >0) {
+			return list.get(0);
+		}
+		return null;
 	}
 }
